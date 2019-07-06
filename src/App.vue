73309f63fa9c362946e04b3e0623ed89cd9adc1f
Matthew Kanderka <matthew.kanderka@ucalgary.ca>
2019-07-06 01:38:36 -0600

    trying to fix routing

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@
       <b-collapse is-nav id="nav_collapse">
 
         <b-navbar-nav>
-          <b-nav-item :to="{name: 'Services'}">Services</b-nav-item>
-          <b-nav-item :to="{name: 'Q&A'}">Q & A</b-nav-item>
+          <router-link :to="{name: 'Services'}"><b-nav-item>Services</b-nav-item></router-link>
+          <router-link :to="{name: 'Q&A'}"><b-nav-item>Q & A</b-nav-item></router-link>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item :to="{name: 'Contact'}">Contact Us</b-nav-item>
+          <router-link><b-nav-item :to="{name: 'Contact'}">Contact Us</b-nav-item></router-link>
         </b-navbar-nav>
 
       </b-collapse>
