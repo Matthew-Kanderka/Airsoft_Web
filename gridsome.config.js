fb503d790328cb1207f090be6732d9dbeceff844
Matthew Kanderka <matthew.kanderka@ucalgary.ca>
2019-08-08 20:29:46 -0600

    test service worker 2

--- gridsome.config.js
+++ gridsome.config.js
@@ -9,25 +9,25 @@ const {GenerateSW} = require('workbox-webpack-plugin');
 module.exports = {
   siteName: 'Humming Bird Airsoft Solutions',
   plugins: [
-    // {
-    //   use: 'gridsome-plugin-pwa',
-    //   options: {
-    //     title: 'Hummingbird Airsoft',
-    //     startUrl: '/',
-    //     display: 'standalone',
-    //     statusBarStyle: 'default',
-    //     manifestPath: 'manifest.json',
-    //     serviceWorkerPath: 'service-worker.js',
-    //     shortName: 'Gridsome',
-    //     themeColor: '#666600',
-    //     backgroundColor: '#ffffff',
-    //     icon: 'public/favicon.ico' // must be supplied!
-    //   }
-    // }
-  ],
-  configureWebpack: {
-    plugins: [
-    new GenerateSW()
-    ]
-  }
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+        title: 'Hummingbird Airsoft',
+        startUrl: '/',
+        display: 'standalone',
+        statusBarStyle: 'default',
+        manifestPath: 'manifest.json',
+        serviceWorkerPath: 'service-worker.js',
+        shortName: 'Gridsome',
+        themeColor: '#666600',
+        backgroundColor: '#ffffff',
+        icon: 'public/favicon.ico' // must be supplied!
+      }
+    }
+  ]
+  // configureWebpack: {
+  //   plugins: [
+  //   new GenerateSW()
+  //   ]
+  // }
 }
